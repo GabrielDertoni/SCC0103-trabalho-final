21a2b0027ffdf37a08046977ba45a73b36c5b341
GabrielDertoni <gab.dertoni@usp.br>
2021-07-22 14:44:25 -0300

    Add entry point

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+import menus.WindowManager;
+
+import java.awt.*;
+
+public class Main {
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                WindowManager.getInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
