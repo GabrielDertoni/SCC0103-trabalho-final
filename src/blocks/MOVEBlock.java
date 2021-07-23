7d191c4046a6549e08b84790b2f742253ee6bd29
GabrielDertoni <gab.dertoni@usp.br>
2021-07-22 22:23:25 -0300

    Better name, use lambda functions

--- src/blocks/MOVEBlock.java
+++ src/blocks/MOVEBlock.java
@@ -21,31 +21,25 @@ public class MOVEBlock extends BaseBlock {
 		this.father = father;
 		
   	  	String directions[] = {"a Direita", "Cima", "a Esquerda", "Baixo"};
-        JComboBox<String> dir = new JComboBox<String>(directions);
-        
-        dir.addActionListener(new ActionListener() {  
+        JComboBox<String> directionPicker = new JComboBox<String>(directions);
 
-        	@Override
-      	  	public void actionPerformed(ActionEvent e) {
-      		  	JComboBox<String> cb = (JComboBox<String>) e.getSource();
-      		  	String dir = (String)cb.getSelectedItem();
-		    
-      		  	switch(dir) {
-      		  		case "a Direita":
-      		  			direction = Direction.RIGHT;
-      		  			break;
-      		  		case "Cima":
-      		  			direction = Direction.UP;
-      		  			break;
-      		  		case "a Esquerda":
-      		  			direction = Direction.LEFT;
-      		  			break;
-      		  		case "Baixo":
-      		  			direction = Direction.DOWN;
-      		  			break;
-      		  	}
-      		  	// Teste: System.out.println(direction);
-      	  }
+		directionPicker.addActionListener(e -> {
+			String dir = (String)directionPicker.getSelectedItem();
+
+			switch(dir) {
+				case "a Direita":
+					direction = Direction.RIGHT;
+					break;
+				case "Cima":
+					direction = Direction.UP;
+					break;
+				case "a Esquerda":
+					direction = Direction.LEFT;
+					break;
+				case "Baixo":
+					direction = Direction.DOWN;
+					break;
+			}
         });
 		
 		JButton removeButton = new JButton("Remover");
@@ -55,7 +49,7 @@ public class MOVEBlock extends BaseBlock {
 		});
         
         add(new JLabel("Mova para"));
-        add(dir);
+        add(directionPicker);
         add(removeButton);
 	}
 
