954987022b9bc6e997956aa88c2f9ced51fda973
Leonardo Chagas Pizzo <leonardo_pizzo@usp.br>
2021-07-27 16:33:56 -0300

    Rewrite Tutorial

--- src/menus/Tutorial.java
+++ src/menus/Tutorial.java
@@ -31,19 +31,61 @@ public class Tutorial extends Background {
 
         add(btnFechar);
 
-        String message = "<html>Ajude Not_Neo a chegar ao computador para que ele possa terminar seu projeto final de" +
-                " Programação Orientada a Objetos. Para isso crie um algoritmo com os blocos de lógica para navegar o " +
-                "labirinto que é seu quarto. O bloco MOVE é usado para movimentar Not_Neo na direção selecionada," +
-                " enquanto o bloco INTERAGIR faz ele interagir com o que estiver na posição do mapa que ele ocupa." +
-                " Os blocos SE e REPETE são especiais. O bloco REPETE faz com que as ações dentro dele sejam repetidas " +
-                "o número de vezes selecionadas e as ações dentro do bloco se só são realizadas se a condição dele " +
+        String message = "Ajude Not_Neo a chegar ao computador para que ele possa terminar seu projeto final de" +
+                " Programacao ";
+        String message1 = "Orientada a Objetos. ";
+        String par = "Para isso crie um algoritmo com os blocos de logica para navegar o " +
+                "labirinto que e seu";
+        String message2 = "quarto. O bloco MOVE e usado para movimentar Not_Neo na direcao selecionada," +
+                " enquanto o bloco";
+        String message3 = "INTERAGIR faz ele interagir com o que estiver na posicao do mapa que ele ocupa." +
+                " Os blocos SE e REPETE";
+        String message4 = "sao especiais. O bloco REPETE faz com que as acoes dentro dele sejam repetidas " +
+                "o numero de vezes";
+        String message5 = "selecionadas e as acoes dentro do bloco SE so sao realizadas se a condicao dele " +
                 "for verdade.";
         JLabel lvlTxt1 = new JLabel(message);
         int label_w = width-50;
         int label_h = height - 300;
         lvlTxt1.setForeground(Color.GREEN.darker());
-        lvlTxt1.setBounds(25, 100, label_w, label_h);
-        lvlTxt1.setFont(whiteRabbit.deriveFont(30f));
+        lvlTxt1.setBounds(25, 20, label_w, label_h);
+        lvlTxt1.setFont(whiteRabbit.deriveFont(25f));
         add(lvlTxt1);
+
+        JLabel parTxt = new JLabel(par);
+        parTxt.setForeground(Color.GREEN.darker());
+        parTxt.setBounds(25, 150, label_w, label_h);
+        parTxt.setFont(whiteRabbit.deriveFont(25f));
+        add(parTxt);
+
+        JLabel lvlTxt2 = new JLabel(message1);
+        lvlTxt2.setForeground(Color.GREEN.darker());
+        lvlTxt2.setBounds(25, 70, label_w, label_h);
+        lvlTxt2.setFont(whiteRabbit.deriveFont(25f));
+        add(lvlTxt2);
+
+        JLabel lvlTxt3 = new JLabel(message2);
+        lvlTxt3.setForeground(Color.GREEN.darker());
+        lvlTxt3.setBounds(25, 200, label_w, label_h);
+        lvlTxt3.setFont(whiteRabbit.deriveFont(25f));
+        add(lvlTxt3);
+
+        JLabel lvlTxt4 = new JLabel(message3);
+        lvlTxt4.setForeground(Color.GREEN.darker());
+        lvlTxt4.setBounds(25, 250, label_w, label_h);
+        lvlTxt4.setFont(whiteRabbit.deriveFont(25f));
+        add(lvlTxt4);
+
+        JLabel lvlTxt5 = new JLabel(message4);
+        lvlTxt5.setForeground(Color.GREEN.darker());
+        lvlTxt5.setBounds(25, 300, label_w, label_h);
+        lvlTxt5.setFont(whiteRabbit.deriveFont(25f));
+        add(lvlTxt5);
+
+        JLabel lvlTxt6 = new JLabel(message5);
+        lvlTxt6.setForeground(Color.GREEN.darker());
+        lvlTxt6.setBounds(25, 350, label_w, label_h);
+        lvlTxt6.setFont(whiteRabbit.deriveFont(25f));
+        add(lvlTxt6);
     }
 }
